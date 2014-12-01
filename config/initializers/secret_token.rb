9771cb4a67e0c2c9c65ac4b2232f37dfb67f3f49
Ryan F. Salerno <rfsalerno@gmail.com>
2014-11-30 19:11:50 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prospectingzen::Application.config.secret_key_base = '05dc44cc61d2bd28fc647e0d30b56494c60843676281f166ddce01e3f162dbe51185cd93af7fb3f870691b98ff4ddc2cdc3bcd7bdc489284f20c573fc3160ac7'
