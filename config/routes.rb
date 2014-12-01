cd2dd98ea608989f70df89bc4c63e1fc4dd161ac
Ryan F. Salerno <rfsalerno@gmail.com>
2014-11-30 21:34:17 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Prospectingzen::Application.routes.draw do
   get 'feedback' => 'pages#feedback'
 
-  root :to => 'pages#index'
+  root :to => 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
